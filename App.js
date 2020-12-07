f75a20dbea9e481a8be1fe9293418ada61b82a6a
zheli1001 <li.zhe5@northeastern.edu>
2020-12-07 02:13:18 -0800

    search page
    search page

--- App.js
+++ App.js
@@ -1,22 +1,113 @@
 import React, { useState } from 'react';
-import { Container } from 'react-bootstrap'
-import useFetchJobs from './useFetchJobs'
+import { Form, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Job from './Job'
+import Axios from 'axios'
 
-function App() {
-    const [params, setParams] = useState()
-    const [page, setPage] = useState()
-    const{jobs, loading} = useFetchJobs(params, page)
-  return (
-      <Container>
-          {loading && <h1> loading... </h1>}
-          {/* {error && <h1> Error </h1>} */}
-          {jobs.map(job => {
-              return <Job key = {job.id} job = {job}></Job>
-          })}
-          {jobs.length}
-      </Container>
-  )
+
+var des;
+var loc;
+const BASE_URL = 'http://localhost:8010/proxy/positions.json'
+var real = 'jobs.github.com/positions.json'
+//positions?description=python&location=new+york
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            url: BASE_URL,
+            jobs: [],
+            description:"",
+            location: "",
+        }
+        this.componentWillMount = this.componentWillMount.bind(this);
+        
+    }
+    myChangeHandlerfordes = event => {
+        des = event.target.value;
+        
+        this.setState({ description: des});
+        // console.log(this.state.description);
+    };
+
+    myChangeHandlerforloc = event => {
+        loc = event.target.value;
+        this.setState({ location: loc });
+    };
+
+    handleClick = () => {
+        var url = BASE_URL;
+        
+        
+        if (loc != null) {
+            this.setState({ location: loc });
+        }
+        if (des != null) {
+            // console.log(des);
+            this.setState({ description: des });
+            // console.log(this.state.description);
+        }
+        url += "&description=" + this.state.description + "&location=" + this.state.location
+        real += "?description=" + this.state.description + "&location=" + this.state.location
+        console.log(real);
+        Axios.get(url)
+        
+        .then(response => {
+            this.setState({jobs:response.data});
+            // console.log(response.data);
+        })
+        
+
+    }
+    componentWillMount() {
+        var url = BASE_URL;
+        
+        Axios.get(url)
+        
+        .then(response => {
+            this.setState({jobs:response.data});
+            // console.log(response.data);
+        })
+    
+    }
+
+
+    render() {
+        console.log("Again");
+        return (
+            <div>
+        <form className="mb-4">
+      <Form.Row className="align-items">
+        <Form.Group as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control  onChange={this.myChangeHandlerfordes} name="description" type="text" />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control onChange={this.myChangeHandlerforloc} name="location" type="text" />
+        </Form.Group>
+        <button width = "50000px" type="button" class="btn btn-primary" onClick={this.handleClick}>
+                    Search
+                </button>
+        {/* <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full-time" label="Only Full Time" type="checkbox" className="mb-2" />
+        </Form.Group> */}
+      </Form.Row>
+      
+    </form>
+            {/* <Pagination page={page} setPage={setPage}></Pagination> */}
+            {this.state.jobs.map(
+                (job) => {
+                    return <Job key={job.id} job={job} />
+                }
+            )}
+            {/* <Pagination page={page} setPage={setPage}></Pagination> */}
+            </div>
+            
+            )
+        
+        }
+    
 }
+
 
 export default App;
--- Job.js
+++ Job.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { ReactComponent } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Job({job}) {
+var jo;
+export default function Job({ job }) {
+    jo = job.how_to_apply;
+    // console.log(jo);
+
     return (
-        <div>
-            {job.title}
-            {job.company_url}
+        <Card className="mb-3">
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+            <a href={job.company_url}>{job.title} - <span className="text-muted font-weight-light">{job.company}</span></a>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="primary" className="mr-2">{job.type}</Badge>
+            <Badge variant="primary">{job.location}</Badge>
+            <div style = {{wordBreak: 'break-all'}}>
+               {job.how_to_apply}
+            </div>
+          </div>
+          <img height="60" alt={job.company} src={job.company_logo} />
         </div>
+        
+      </Card.Body>
+    </Card>
     )
+    
 }
